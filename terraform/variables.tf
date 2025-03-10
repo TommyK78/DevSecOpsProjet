75e0dba34ee0e246860ed010e94da45462c5eac0
TommyK78 <tommy.k78@example.com>
2025-03-10 16:07:37 +0100

    Update Terraform configuration and gitignore

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,41 +1,17 @@
 variable "aws_region" {
-  description = "Région AWS pour le déploiement"
+  description = "AWS region"
   type        = string
   default     = "eu-west-1"
 }
 
-variable "environment" {
-  description = "Environnement de déploiement"
-  type        = string
-  default     = "production"
-}
-
-variable "vpc_cidr" {
-  description = "CIDR block pour le VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "instance_type" {
-  description = "Type d'instance EC2"
-  type        = string
-  default     = "t2.micro"
-}
-
-variable "volume_size" {
-  description = "Taille du volume root en GB"
-  type        = number
-  default     = 30
-}
-
 variable "aws_access_key" {
-  description = "AWS Access Key"
+  description = "AWS access key"
   type        = string
   sensitive   = true
 }
 
 variable "aws_secret_key" {
-  description = "AWS Secret Key"
+  description = "AWS secret key"
   type        = string
   sensitive   = true
 }
